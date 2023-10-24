96c39773e41934843454cef0b7b7e49b3ac92b32
Amadeus Reger <AmadeusReger@gmx.de>
2023-10-24 20:58:37 +0200

    shaders and so on

--- nodes/Game/eye.gd
+++ nodes/Game/eye.gd
@@ -5,12 +5,14 @@ var start_postion:Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start_postion = global_position
+	start_postion = position
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	#mouse - start Position
-	var richtung = get_viewport().get_mouse_position() - start_postion
-	var length = clamp(richtung.length(),0,radius) 
-	global_position = start_postion + richtung.normalized() * length
+	var richtung = get_viewport().get_mouse_position() - global_position
+	var length = clamp(richtung.length(),0,radius)
+	position = start_postion + richtung.normalized() * length
+	#global_position = start_postion + richtung.normalized() * length
